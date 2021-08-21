ef1a808f452fda039b029195c0be4e0c7df4c9fd
joshuabrink1 <joshuabrink1@gmail.com>
2021-08-21 13:45:49 +0200

    Revert "Absolute path resolving for relative path input"
    This reverts commit 461792a0ad4604f2cc6bea1e901dc6d48bdf68ac.

--- src/buffer_manager.ts
+++ src/buffer_manager.ts
@@ -1,7 +1,6 @@
 import { debounce } from "lodash-es";
 import { Buffer, NeovimClient, Window } from "neovim";
 import { ATTACH } from "neovim/lib/api/Buffer";
-import path from "path";
 import {
     commands,
     Disposable,
@@ -190,16 +189,16 @@ export class BufferManager implements Disposable, NeovimRedrawProcessable, Neovi
                 const [fileName, close] = args as [string, number | "all"];
                 const currEditor = window.activeTextEditor;
                 let doc: TextDocument | undefined;
-                try {
-                    if (fileName === "__vscode_new__") {
-                        doc = await workspace.openTextDocument();
+                if (fileName === "__vscode_new__") {
+                    doc = await workspace.openTextDocument();
+                } else {
+                    const normalizedName = fileName.trim();
+                    const uri = await this.getInternalUri(normalizedName);
+                    if (uri) {
+                        doc = await workspace.openTextDocument(uri);
                     } else {
-                        const normalizedName = fileName.trim()
-                        const filePath = this.findPathFromFileName(normalizedName);
-                        doc = await workspace.openTextDocument(filePath);
+                        doc = await workspace.openTextDocument(normalizedName);
                     }
-                } catch (error) {
-                    this.logger.error(`${LOG_PREFIX}: Error opening file ${fileName}, ${error}`);
                 }
                 if (!doc) {
                     return;
@@ -594,15 +593,13 @@ export class BufferManager implements Disposable, NeovimRedrawProcessable, Neovi
         return false;
     }
 
-    private findPathFromFileName(name: string): string {
-        const rootFolderPath = workspace.workspaceFolders![0].uri.fsPath;
-        let filePath: string;
-        if (rootFolderPath) {
-            filePath = path.resolve(rootFolderPath, name);
-        } else {
-            filePath = name;
+    private async getInternalUri(name: string): Promise<Uri | undefined> {
+        const fileUris = await workspace.findFiles('**/' + name)
+        const fileUri = fileUris[0]
+        if (fileUri) {
+            return fileUri
         }
-        return filePath;
+        return undefined;
     }
 
     private findDocFromUri(uri: string): TextDocument | undefined {
